fd071123f5d7aa31635e8dcf888062a937704fc7
DarkhanSol0 <darkhansol069@gmail.com>
2026-03-26 17:39:24 +0900

    add

--- efi-system/a.bash
+++ efi-system/a.bash
@@ -1,5 +1,5 @@
 # Более продвинутая версия для bash
 find . -name "*.md" -type f | sort | while read file; do
-    echo -e "\n\n# 📄 ${file}\n" >> 123.md
-    cat "$file" >> 123.md
+    echo -e "\n\n# 📄 ${file}\n" >> gsys.md
+    cat "$file" >> gsys.md
 done
--- efi-system/Механики/Пути/a.bash
+++
@@ -1,5 +0,0 @@
-# Более продвинутая версия для bash
-find . -name "*.md" -type f | sort | while read file; do
-    echo -e "\n\n# 📄 ${file}\n" >> 123.md
-    cat "$file" >> 123.md
-done
